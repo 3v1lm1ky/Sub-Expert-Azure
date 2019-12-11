994161b596b41efe1ad6b7d99bc93710ed919246
3v1lm1ky <endemictrojantrafox@gmail.com>
2019-12-11 15:57:25 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName="mactologywebapp";AccountKey="v4oZVbmnbqmLmBgiDnZGAI5jmdybXQninkBFWBPH44U+Y5xwoWzgRSLRYrkG6g/GnMqaxGeNpjPj/UupPN+iDw==";
+$containerName = "blockblobshsgmut";
+
+
+
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+if (isset($_POST['submit'])) {
+	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
+	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+	// echo fread($content, filesize($fileToUpload));
+	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+	header("Location: index.php");
+	
+}
+
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+ 
+
+    <title>Blob Uploader</title>
+
+  </head>
+<body>
+	<main role="main" class="container">
+    		<div class="starter-template"> <br>
+        		<h1 align="center">Blob Uploader</h1><br>
+				<p class="lead">Please choose a image. and then click button <b>Upload</b>
+				<span class="border-top my-3"></span>
+			</div>
+		<div class="mt-4 mb-2">
+			<form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png,.gif" required="">
+				<input type="submit" name="submit" value="Upload">
+			</form>
+			
+		</div>
+		<br>
+		<br>
+		<h4>List Files :</h4>
+		<table class='table table-hover' align="center">
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
+						?>
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="ImageAnalyze.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
+								</form>
+							</td>
+						</tr>
+						<?php
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
+
+	</div>
+
+  </body>
+</html>
