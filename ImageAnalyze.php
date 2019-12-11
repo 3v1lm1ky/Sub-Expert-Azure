4e4372de3c48bd9866daf3568be7e8becfa44160
3v1lm1ky <endemictrojantrafox@gmail.com>
2019-12-11 16:55:18 +0700

    Update ImageAnalyze.php

--- ImageAnalyze.php
+++ ImageAnalyze.php
@@ -29,7 +29,7 @@ if (isset($_POST['submit'])) {
         var subscriptionKey = "26a26d06a0e54537b640d6c034b0d70c";
  
         var uriBase =
-            "https://mactologyvision.cognitiveservices.azure.com/vision/v2.0/analyze";
+            "https://mactologyvision.cognitiveservices.azure.com/";
  
         var params = {
             "visualFeatures": "Categories,Description,Color",
